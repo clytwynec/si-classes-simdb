b9222df3ba91502be292604ccd5c8157ebf90eff
Bob Breznak <bob.breznak@gmail.com>
2013-03-22 06:15:18 -0400

    Add validation test for Movie title

--- test/unit/movie_test.rb
+++ test/unit/movie_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class MovieTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "movies require a title" do
+    movie = build(:movie, title: nil)
+    assert !movie.save, "could save movie without having a title"
+    assert !movie.errors.blank?
+    assert_equal 1, movie.errors.count
+    assert_equal ["can't be blank"], movie.errors[:title]
+
+    movie.title = 'Some title'
+    assert movie.save, "could not save a valid movie"
+  end
 end
